e4b734365e5c0e934fd07265ae0e8ba36dd342e6
Amlan Das <dasamlan271988@gmail.com>
2024-03-25 18:44:05 +0530

    terraform change

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "dasa123" {
+resource "aws_instance" "dasa2024" {
     ami = "ami-00e87074e52e6c9f9"
     instance_type = "t2.micro"
 }
