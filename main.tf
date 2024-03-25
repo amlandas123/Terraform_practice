7a4a79a4169c67ad4f8b4a5f144d0e65de55a3cb
Amlan Das <dasamlan271988@gmail.com>
2024-03-25 19:37:11 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -5,9 +5,28 @@ provider "aws" {
 resource "aws_instance" "dasa2024" {
     ami = "ami-00e87074e52e6c9f9"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
     
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
+
     tags = {
         Name = "dasa_practice"
     }    
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform_practice_sg"
+  
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
