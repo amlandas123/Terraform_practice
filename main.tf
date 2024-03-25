a56b9e6c97d1e9cc7649009a5150a125fb2374b4
Amlan Das <dasamlan271988@gmail.com>
2024-03-25 20:00:19 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "instance" {
     ingress {
         from_port = 8080
         to_port = 8080
-        protocol = "All traffic"
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
