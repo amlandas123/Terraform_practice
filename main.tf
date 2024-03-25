a5be32378b997ab06bbf099531e5ece7f896b7fb
Amlan Das <dasamlan271988@gmail.com>
2024-03-25 19:19:08 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ provider "aws" {
 resource "aws_instance" "dasa2024" {
     ami = "ami-00e87074e52e6c9f9"
     instance_type = "t2.micro"
+    
+    tags = {
+        Name = "dasa_practice"
+    }    
 }
 
-tags = {
-    Name = "dasa-practice"
-}
