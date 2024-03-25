5ec56cdab75dd5aebc762441e23e8e193b444b3c
Amlan Das <dasamlan271988@gmail.com>
2024-03-25 20:43:03 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "instance" {
     name = "terraform_practice_sg"
   
     ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "All"
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
