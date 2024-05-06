5c44726f2a8739d368eecfeee6a689df828c6369
Amlan Das <dasamlan271988@gmail.com>
2024-05-06 23:06:01 +0530

    From amlan

--- main.tf
+++ main.tf
@@ -2,31 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "dasa2024" {
-    ami = "ami-0f75a13ad2e340a58"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.sg-demo.id]
-    
-    user_data = <<-EOF
-                #!/bin/bash
-                sudo yum install httpd
-                sudo systemctl enable httpd
-                sudo systemctl start httpd
-                echo "Hello World" > /var/www/html/index.html
-                EOF
-
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "dasa_practice"
-    }    
-}
 variable "tcp_port" {
     description = "Used to open port"
     type = number
     default = 8080
 }
-
 
 resource "aws_security_group" "sg-demo" {
     name = "terraform_practice_sg"
@@ -39,3 +19,32 @@ resource "aws_security_group" "sg-demo" {
     }
 
 }
+
+resource "aws_launch_configuration" "dasa2024" {
+    image_id = "ami-0f75a13ad2e340a58"
+    instance_type = "t2.micro"
+    security_groups = [ aws_security_group.sg-demo.id ]
+    
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f ${var.tcp_port}  &
+                EOF
+
+}    
+
+resource "aws_autoscaling_group" "dasa-asg" {
+    launch_configuration = aws_launch_configuration.dasa2024.name
+    min_size = 2
+    max_size = 6
+    tag {
+      key = "Name"
+      value = "terraform as example"
+      propagate_at_launch = true
+    }
+
+
+}
+
+
+
